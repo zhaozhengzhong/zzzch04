d8bbbbec11c04932a9a0df3d5de177fae36d0dcd
mxhsb <1010195505@qq.com>
2020-04-16 17:14:25 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ch0401';
+  shouldShow = true;
+  score = 60;
+  color: string;
+  fontsize: string;
+  isBordered = true;
+  classObj = {
+    bordered: this.isBordered,
+    blue: false,
+    round: true,
+  }
+  cities = ['杭州', '南京', '上海', '无锡'];
+  onT() {
+    this.shouldShow = !this.shouldShow;
+  }
+  isB() {
+    this.isBordered = !this.isBordered;
+  }
+  grade() {
+    return Math.floor(this.score / 10);
+  }
+  onI(evt: Event) {
+    const Ipt = <HTMLInputElement>evt.target;
+    this.score = Number(Ipt.value);
+  }
+  apply(color: string, fontsize: string) {
+    this.color = color;
+    this.fontsize = fontsize;
+  }
 }
